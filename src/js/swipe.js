d15d580390061205c9bf4dccb3edcfb0bf89eb9f
Fabian Kuffer <fabian.kuffer@kuffer.info>
2021-10-30 18:52:47 +0200

    spotify with promises

--- src/js/swipe.js
+++ src/js/swipe.js
@@ -1,7 +1,14 @@
 window.onload = function () {
     initPopups();
     initLogout();
-    checkIfConnected();
+    checkConnected().then(
+        function(){
+            checkAccessToken();
+        },
+        function(){
+
+        }
+    );
 };
 
 function initLogout(){
@@ -23,61 +30,69 @@ function logout(){
     xhttp.send();
 }
 
-function checkIfConnected(){
-    let spotifyLogin = document.getElementById("swipe-login-spotify");
-    let cart = document.getElementById("swipe-cart-container");
-    let spotifyButton = document.getElementById("spotify-login-button");
-
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4){
-            if(this.status == 200) {
-                let response = JSON.parse(xhttp.responseText);
-                if(response.connected){
-                    spotifyLogin.style.display = "none";
-                    cart.style.display = "block";
-                    checkAccessToken();
+function checkConnected(){
+    return new Promise(function(resolve,reject){
+        let spotifyLogin = document.getElementById("swipe-login-spotify");
+        let cart = document.getElementById("swipe-cart-container");
+        let spotifyButton = document.getElementById("spotify-login-button");
+    
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4){
+                if(this.status == 200) {
+                    let response = JSON.parse(xhttp.responseText);
+                    if(response.connected){
+                        spotifyLogin.style.display = "none";
+                        cart.style.display = "block";
+                        resolve();
+                    } else {
+                        spotifyLogin.style.display = "block";
+                        spotifyButton.addEventListener("click",function(){
+                            window.location.href = response.link;
+                        });
+                        cart.style.display = "none";
+                        reject();
+                    }
                 } else {
                     spotifyLogin.style.display = "block";
-                    spotifyButton.addEventListener("click",function(){
-                        window.location.href = response.link;
-                    });
                     cart.style.display = "none";
+                    spotifyLogin.innerText = "An error occured please reload the site";
+                    reject();
                 }
-            } else {
-                spotifyLogin.style.display = "block";
-                cart.style.display = "none";
-                spotifyLogin.innerText = "An error occured please reload the site";
             }
-        }
-    };
-    xhttp.open("post", "/user/connected", true);
-    xhttp.send();
+        };
+        xhttp.open("post", "/user/connected", true);
+        xhttp.send();
+    });
 }
 
-function checkAccessToken(){
+async function checkAccessToken(){
     if(localStorage.accesstoken && localStorage.expireDate){
         if(localStorage.expireDate < Date.now()){
-            requestAccessToken();
+            await requestAccessToken();
         }
     } else {
-        requestAccessToken();
+        await requestAccessToken();
     }
 }
 
 function requestAccessToken(){
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4){
-            if(this.status == 200) {
-                let response = JSON.parse(xhttp.responseText); 
-                localStorage.setItem("accesstoken", response.access_token);
-                localStorage.setItem("expireDate", response.expires);
-            } else {
-                console.log("error no new accesstoken");
+    return new Promise(function(resolve, reject){
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4){
+                if(this.status == 200) {
+                    let response = JSON.parse(xhttp.responseText); 
+                    localStorage.setItem("accesstoken", response.access_token);
+                    localStorage.setItem("expireDate", response.expires);
+                    resolve();
+                } else {
+                    console.log("error no new accesstoken");
+                    reject();
+                }
             }
-        }
-    };
-    xhttp.open("get", "/get/accesstoken", true);
-    xhttp.send();
+        };
+        xhttp.open("get", "/get/accesstoken", true);
+        xhttp.send();
+    });
 }
