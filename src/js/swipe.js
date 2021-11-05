abc30b68a8f1c9fde8eae6616d3298806f94526e
Fabian Kuffer <fabian.kuffer@kuffer.info>
2021-11-05 18:23:30 +0100

    snackbar added

--- src/js/swipe.js
+++ src/js/swipe.js
@@ -188,3 +188,13 @@ async function initSuggestion(){
         document.getElementById("swipe-popup-all-songs").checked = true;
     }
 }
+
+function displayInSnackbar(text){
+    let snackbar = document.getElementById("snackbar");
+    snackbar.innerText = text;
+    let calcmargin = -(snackbar.offsetWidth / 2);
+    snackbar.style.marginLeft = calcmargin+"px";
+    snackbar.classList.add("showSnackbar");
+
+    setTimeout(function(){ snackbar.className = snackbar.classList.remove("showSnackbar"); }, 3000);
+}
